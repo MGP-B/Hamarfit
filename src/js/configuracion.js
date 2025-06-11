df544a30d4a252dc8b344c888a8c6b8d862617a1
Winnar <winnarperalta3@gmail.com>
2025-06-11 15:46:20 -0400

    función de agregar pop ups

--- src/js/configuracion.js
+++ src/js/configuracion.js
@@ -1,14 +1,25 @@
-const boton_abrir = document.getElementById('nuevo_usuario');
-const popup = document.getElementById('cuerpo_popup')
-const cuerpo = document.getElementById('cuerpo')
+// Crear una constante que llame al contenedor del pop up
+const contenedor_popup = document.getElementById('contenedor_popup')
 
-boton_abrir.addEventListener('click', async ()=>{
-    const respuesta = (await fetch('/html/admin/desplegables/configuracion/nuevo_usuario.html')).json;
-    const contenido = respuesta.toString();
-    
-    const contenedor = document.createElement('div');
-    contenedor.style.padding='100px';
-    contenedor.style.backgroundColor='red';
+// Crear la función para agregar los pop ups
+function agregar_popup(ruta){
+    // Buscar en la ruta especificada
+    fetch(ruta)
 
-    alert('respuesta: '+contenido)
-})
+    // Luego
+    .then(respuesta =>{
+        // Retornar la respuesta en texto
+        return respuesta.text();
+    })
+
+    // Luego
+    .then(contenido=>{
+        // Agregar el contenido de la respuesta al contenedor del pop up
+        contenedor_popup.innerHTML = contenido;
+    })
+    // En caso de error atrapa...
+    .catch(error=>{
+        // Agregar el mensaje de error al contenedor del pop up
+        contenedor_popup.innerHTML = 'Error => '+ error + 'Qué hiciste? ya se dañó';
+    })
+};
