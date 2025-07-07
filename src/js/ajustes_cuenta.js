67a7da1f2664043def1682347fecea89b86af3c5
Winnar <winnarperalta3@gmail.com>
2025-07-07 16:06:16 -0400

    correcciones al js y css de ajustes de cuenta

--- src/js/ajustes_cuenta.js
+++ src/js/ajustes_cuenta.js
@@ -1,22 +1,29 @@
 // Evento que se va a ejecutar cuando el documento esté completamente cargado
 document.addEventListener('DOMContentLoaded',()=>{
+    // Obtener los contenedores
     const cambio_contrasena = document.getElementById('cambio_contrasena');
     const cambio_correo = document.getElementById('cambio_correo');
     const cambio_telefono = document.getElementById('cambio_telefono');
 
-
+    // Obtener los botones
     const boton_contrasena = document.getElementById('boton_contrasena');
     const boton_correo = document.getElementById('boton_correo');
     const boton_telefono = document.getElementById('boton_telefono');
 
-
+    // Obtener los input
     const input_contrasena = document.getElementById('input_contrasena');
     const input_telefono = document.getElementById('input_telefono');
     const input_correo = document.getElementById('input_correo');
     const input_confirmar_contrasena = document.getElementById('confirmar-contrasena');
 
+    // Evento al botón de contraseña
     boton_contrasena.addEventListener('click',()=>{
-        cambio_contrasena.style.display = 'inline-flex';
+        if(cambio_contrasena.style.display === 'none'){
+            cambio_contrasena.style.display = 'inline-flex';
+            input_contrasena.focus()
+        }else{
+            cambio_contrasena.style.display = 'none';
+        }
 
         if (cambio_correo.style.display === 'inline-flex' || cambio_telefono.style.display === 'inline-flex'){
             cambio_correo.style.display = 'none';
@@ -24,13 +31,18 @@ document.addEventListener('DOMContentLoaded',()=>{
 
             input_correo.value = '';
             input_telefono.value = '';
-        }else{
-            console.log('Los otros contenedores ya tienen display none')
         }
     })
 
+    // Evento al botón del correo
     boton_correo.addEventListener('click',()=>{
-        cambio_correo.style.display = 'inline-flex';
+        
+        if(cambio_correo.style.display === 'none'){
+            cambio_correo.style.display = 'inline-flex';
+            input_correo.focus()
+        }else{
+            cambio_correo.style.display = 'none';
+        }
 
         if (cambio_contrasena.style.display === 'inline-flex' || cambio_telefono.style.display === 'inline-flex'){
             cambio_contrasena.style.display = 'none';
@@ -39,13 +51,18 @@ document.addEventListener('DOMContentLoaded',()=>{
             input_telefono.value = '';
             input_confirmar_contrasena.value = '';
             input_contrasena.value = '';
-        }else{
-            console.log('Los otros contenedores ya tienen display none')
         }
     })
 
+    // Evento al botón de telefono
     boton_telefono.addEventListener('click',()=>{
-        cambio_telefono.style.display = 'inline-flex';
+
+        if(cambio_telefono.style.display === 'none'){
+            cambio_telefono.style.display = 'inline-flex';
+            input_telefono.focus()
+        }else{
+            cambio_telefono.style.display = 'none'; 
+        }
 
         if (cambio_correo.style.display === 'inline-flex' || cambio_contrasena.style.display === 'inline-flex'){
             cambio_correo.style.display = 'none';
@@ -54,8 +71,6 @@ document.addEventListener('DOMContentLoaded',()=>{
             input_correo.value = '';
             input_confirmar_contrasena.value = '';
             input_contrasena.value = '';
-        }else{
-            console.log('Los otros contenedores ya tienen display none')
         }
     })
 })
